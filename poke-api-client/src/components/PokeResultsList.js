9aa3be29ad0db79dedbbe6eb66ee13a3355e9087
wr9 <asikim00@fesb.hr>
2017-10-25 21:24:30 +0200

    minor changes

--- poke-api-client/src/components/PokeResultsList.js
+++ poke-api-client/src/components/PokeResultsList.js
@@ -6,7 +6,8 @@ class PokeResultsList extends Component {
     render() {
         return (
             <div>
-                {this.props.pokemon.length &&
+                <h2>Pokemon</h2>
+                {this.props.pokemon.length > 0 &&
                     this.props.pokemon.map((pokemon) =>
                         <div key={pokemon.id}>
                             < Pokemon pokemon={pokemon} />
--- poke-api-client/src/components/Pokemon.js
+++ poke-api-client/src/components/Pokemon.js
@@ -6,16 +6,26 @@ class Pokemon extends Component {
         console.log(this.props.pokemon)
         return (
             <div>
-                Name: {this.props.pokemon.name}
-                Weight: {this.props.pokemon.weight}
-                Height: {this.props.pokemon.height}
-                Types:
+                <h3>Name: {this.props.pokemon.name}</h3>
+                <div>Weight: {this.props.pokemon.weight}</div>
+                <div>Height: {this.props.pokemon.height}</div>
+                <div>
+                    Types:
                 {this.props.pokemon.types.map( type =>
-                    <div key={type}>
-                        {type}
-                    </div>
+                    <span key={type}>
+                        {type + ' '}
+                    </span>
                 )}
-                No of moves: {this.props.pokemon.moves.length}
+                </div>
+                <div>No of moves: {this.props.pokemon.moves.length}</div>
+                <div>
+                    Moves:
+                {this.props.pokemon.moves.map( move =>
+                    <span key={move}>
+                        {move + ' '}
+                    </span>
+                )}
+                </div>
             </div>
         );
     }
